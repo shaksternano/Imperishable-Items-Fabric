9baee1b5c93093b72386f02b9308d6a66faf87ef
Shukai Zhang <shukai.zhang@hotmail.com>
2021-10-19 21:42:27 +0100

    Update to 1.17

--- src/main/java/com/shaksternano/imperishableitems/compat/ModMenuIntegration.java
+++ src/main/java/com/shaksternano/imperishableitems/compat/ModMenuIntegration.java
@@ -1,8 +1,8 @@
 package com.shaksternano.imperishableitems.compat;
 
 import com.shaksternano.imperishableitems.config.ModConfig;
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
