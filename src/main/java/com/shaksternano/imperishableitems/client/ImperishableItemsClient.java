3ddacad4d6ac89498b98710c2fa1f2485f398c9b
Shukai Zhang <shukai.zhang@hotmail.com>
2021-10-26 20:48:12 +0100

    Break effects work

---
+++ src/main/java/com/shaksternano/imperishableitems/client/ImperishableItemsClient.java
@@ -0,0 +1,15 @@
+package com.shaksternano.imperishableitems.client;
+
+import com.shaksternano.imperishableitems.network.ModNetworkHandler;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ImperishableItemsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        ModNetworkHandler.registerClientGlobalReceivers();
+    }
+}
