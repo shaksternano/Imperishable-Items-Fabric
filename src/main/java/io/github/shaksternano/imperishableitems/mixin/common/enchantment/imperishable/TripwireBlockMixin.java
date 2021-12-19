1f2716b1bced14d93adfdbb0976fcc31669c9298
ShakterNano <shukai.zhang@hotmail.com>
2021-12-19 23:11:37 +0000

    Make compatible with BCLip hopefully

--- src/main/java/io/github/shaksternano/imperishableitems/mixin/common/enchantment/imperishable/TripwireBlockMixin.java
+++ src/main/java/io/github/shaksternano/imperishableitems/mixin/common/enchantment/imperishable/TripwireBlockMixin.java
@@ -2,11 +2,9 @@ package io.github.shaksternano.imperishableitems.mixin.common.enchantment.imperi
 
 import io.github.shaksternano.imperishableitems.common.enchantment.ImperishableEnchantment;
 import io.github.shaksternano.imperishableitems.common.util.ImperishableBlacklistsHandler;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TripwireBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -15,23 +13,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TripwireBlock.class)
-abstract class TripwireBlockMixin extends Block {
-
-    public TripwireBlockMixin(Settings settings) {
-        super(settings);
-    }
+abstract class TripwireBlockMixin {
 
     // Shears with Imperishable at 0 durability can't disarm tripwires.
-    @Redirect(method = "onBreak", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
-    private boolean imperishableDisarmTripwire(ItemStack getMainHandStack, Item item, World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (ImperishableBlacklistsHandler.isItemProtected(getMainHandStack, ImperishableBlacklistsHandler.ProtectionType.BREAK_PROTECTION)) {
-            if (!player.isCreative()) {
+    @Redirect(method = "onBreak", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"))
+    private boolean imperishableDisarmTripwire(ItemStack getMainHandStack, World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        if (!player.isCreative()) {
+            if (ImperishableBlacklistsHandler.isItemProtected(getMainHandStack, ImperishableBlacklistsHandler.ProtectionType.BREAK_PROTECTION)) {
                 if (ImperishableEnchantment.isBrokenImperishable(getMainHandStack)) {
-                    return false;
+                    return true;
                 }
             }
         }
 
-        return getMainHandStack.isOf(item);
+        return getMainHandStack.isEmpty();
     }
 }
